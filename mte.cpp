21c1cf966d1285314599aa0e246df656a8b20693
Varun <varun.py10@gmail.com>
2022-01-18 22:48:06 +0530

    added menu

--- mte.cpp
+++ mte.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <conio.h>
 #include<fstream>
+#include<stdlib.h>
 using namespace std;
 
 string password="1234";
@@ -26,6 +27,7 @@ public:
 
 
        file.open("prison.text",ios::app|ios::out);
+
        file<<" "<<serial<<" "<<age<<" "<<time<<" "<<name<<" "<<crime;
        file.close();
 
@@ -33,9 +35,13 @@ public:
     void show(){
         char ch;
         fstream file;
+
         file.open("prison.text",ios::in);
+          while (!file.eof()){
        file>>ch>>serial>>ch>>age>>ch>>time>>ch>>name>>ch>>crime;
-       cout<<" serial: "<<serial<<endl<<" age: "<<age<<endl<<" time: "<<time<<endl<<" Name"<<name<<endl<<" crime"<<crime<<endl;
+       cout<<" serial: "<<serial<<endl<<" age: "<<age<<endl<<" time: "<<time<<endl<<" Name: "<<name<<endl<<" crime: "<<crime<<endl;
+       cout<<endl<<endl;
+          }
        file.close();
 
     }
@@ -58,7 +64,6 @@ bool auth(){
     string s;
     cin>>s;
     if(s==password){
-            cout<<"true";
         return true;
     }
 
@@ -67,6 +72,7 @@ bool auth(){
 }
 
 int main(){
+    int x;
     cout<<"hello to prison management system"<<endl<<"press enter to continue"<<endl;
     getch();
 
@@ -78,8 +84,44 @@ int main(){
     cout<<"correct password"<<endl;
 
     prison s;
+
+    menu:
+    system("CLS");
+    cout<<"Enter choices:"<<endl;
+    cout<<"1.Add prison information:"<<endl;
+    cout<<"2.Find prisoner: "<<endl;
+    cout<<"3.Remove prisoner"<<endl;
+    cout<<"4.show all prisoners value"<<endl;
+    cout<<"5.exit"<<endl;
+    cin>>x;
+    switch(x) {
+  case 1:
+    s.add();
+    goto menu;
+    break;
+  case 2:
+    s.find();
+    goto menu;
+    break;
+  case 3:
+    s.remove();
+    goto menu;
+    break;
+  case 4:
     s.show();
-    //s.add();
+    getch();
+    goto menu;
+    break;
+  case 5:
+    exit(0);
+
+  default:
+      cout<<"enter valid input ";
+      exit(0);
+      goto menu;
+
+    // code block
+}
 
     return 0;
 }
